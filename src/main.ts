04ba4769839fd76ccfcd466c5aabf0195a5e3869
Igor Omelchenko <megapixel23@gmail.com>
2019-12-07 23:14:17 +0200

    Logging added for all incoming messages.

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,20 @@ import * as TelegramBot from 'node-telegram-bot-api';
 import { TELEGRAM_TOKEN } from './config';
 import { addSong } from './handlers';
 import { isCommandMessage } from './util';
+import MessageLogger from './services/MessageLogger';
 
 const bot: TelegramBot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
+const messageLogger: MessageLogger = new MessageLogger();
 
 bot.onText(/^\/start/, (msg: TelegramBot.Message) => {
   const chatId = msg.chat.id;
 
-  return bot.sendMessage(chatId, 'Hey');
+  return bot.sendMessage(chatId, 'Hey!');
 });
 
 bot.on('text', (msg: TelegramBot.Message) => {
+  messageLogger.save(msg);
+
   if (isCommandMessage(msg.text)) {
     return null;
   }
---
+++ src/services/MessageLogger.ts
@@ -0,0 +1,22 @@
+import { DynamoDB } from 'aws-sdk';
+import * as TelegramBot from 'node-telegram-bot-api';
+
+const dynamoDb = new DynamoDB.DocumentClient({
+  apiVersion: '2012-08-10',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+export default class MessageLogger {
+  public save (msg: TelegramBot.Message) {
+    return dynamoDb.put({
+      TableName: 'playlister.messages',
+      Item: {
+        'userId.env': `${msg.from.id}.dev`,
+        datetime: new Date().toISOString(),
+        user: msg.from,
+      }
+    }).promise();
+  }
+}
